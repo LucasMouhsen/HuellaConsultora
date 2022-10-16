1088ec613195e3cb50e3d56d9ab8da904794c1e1
Lucas12github <Lucasmouhsen@gmail.com>
2022-10-16 13:11:23 -0300

    Cambios solicitados por Juan y Ricardo

--- js/index.js
+++ js/index.js
@@ -1,14 +1,44 @@
+ScrollReveal({ reset: true });
+ScrollReveal().reveal(".reveal");
 
 const headerScroll = () => {
-    const header = document.getElementById('header'),
+    const header = document.getElementById('header')
+    const logo = document.getElementById('logo'),
         headerHeight = header.offsetHeight
     window.addEventListener('scroll', () => {
         if (window.scrollY > headerHeight) {
             header.classList.add('header-white')
+            logo.classList.add('logo-active')
         } else {
             header.classList.remove('header-white')
+            logo.classList.remove('logo-active')
         }
     })
 }
 
 headerScroll()
+
+
+
+/* const showMenu = (headerToggle, navbarId) =>{
+    const toggleBtn = document.getElementById(headerToggle),
+    nav = document.getElementById(navbarId)
+    
+    if(headerToggle && navbarId){
+        toggleBtn.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu')
+            toggleBtn.classList.toggle('bx-x')
+        })
+    }
+}
+showMenu('header-toggle','navbar')
+
+const linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('activo'))
+    this.classList.add('activo')
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink))
+ */
